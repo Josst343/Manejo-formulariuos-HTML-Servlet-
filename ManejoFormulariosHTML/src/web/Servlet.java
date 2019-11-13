6234fe02674e84bd2394debeeee80e2f06c7128c
Josst343 <43652757+Josst343@users.noreply.github.com>
2019-11-13 01:34:03 -0600

    Termino vistas falta capa de control modelo y base de datos "jdbc"
    Creacion de un formulario con CSS JavaScript y HTML en un proyuecto java web dynamic

---
+++ ManejoFormulariosHTML/src/web/Servlet.java
@@ -0,0 +1,12 @@
+package web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/Servlet")
+public class Servlet  extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+
+}
